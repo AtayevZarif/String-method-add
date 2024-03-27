3a158a52106b639c32acc152a6c7a665c405ff63
AtayevZarif <atayev1234567890@gmail.com>
2024-03-28 02:58:08 +0500

    Correct

--- SM-10.js
+++ SM-10.js
@@ -1,7 +1,8 @@
 let S1 = prompt("Satr yozing (mainimal uzunligi: 10ta harf)")
 let S2 = prompt("Yana satr yozing (maksimal uzunligi: 10ta harf)")
+let S3 = prompt("Yana satr yozing (maksimal uzunligi: 10ta harf)")
 let boolean = S1.includes(S2)
-let replace = S1.replaceAll(S2,"")
+let replace = S1.replace(S2,S3)
 let length1 = S1.length
 let length2 = S2.length
 
